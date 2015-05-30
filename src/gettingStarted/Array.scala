1cd2589944686bf88cf5c83ffd2c565cfc3048c6
Ashok Gelal <ashokgelal@gmail.com>
2015-05-30 17:01:24 -0600

    More descriptive function name

--- src/gettingStarted/Array.scala
+++ src/gettingStarted/Array.scala
@@ -13,11 +13,11 @@ object Array {
     loop(0)
   }
 
-  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
     @tailrec
     def loop(n: Int): Boolean = {
       if (n >= as.length - 1) true // reached the end
-      else if (ordered(as(n), as(n + 1))) false
+      else if (gt(as(n), as(n + 1))) false
       else loop(n + 1)
     }
     loop(0)
