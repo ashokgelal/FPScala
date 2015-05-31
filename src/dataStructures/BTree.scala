b6c5848d34eec93949ccb6da61e5643ef776810c
Ashok Gelal <ashokgelal@gmail.com>
2015-05-31 14:42:40 -0600

    Implement fold for BTree

--- src/dataStructures/BTree.scala
+++ src/dataStructures/BTree.scala
@@ -13,9 +13,17 @@ object BTree {
     case Branch(l, r) => size(l) + size(r)
   }
 
+  def sizeViaFold[A](t: BTree[A]): Int = {
+    fold(t)(a => 1)(1 + _ + _)
+  }
+
   def maximum(t: BTree[Int]): Int = t match {
     case Leaf(x) => x
     case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
+  def maximumViaFold(t: BTree[Int]): Int = {
+    fold(t)(a => a)(_ max _)
   }
 
   def depth[A](t: BTree[A]): Int = t match {
@@ -23,9 +31,22 @@ object BTree {
     case Branch(l, r) => 1 + depth(l) max depth(r)
   }
 
+  def depthViaFold[A](t: BTree[A]): Int = {
+    fold(t)(_ => 0)((d1, d2) => 1 + (d1 max d2))
+  }
+
   def map[A, B](t: BTree[A])(f: A => B): BTree[B] = t match {
     case Leaf(x) => Leaf(f(x))
     case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def mapViaFold[A, B](t: BTree[A])(f: A => B): BTree[B] = {
+    fold(t)(a => Leaf(f(a)): BTree[B])(Branch(_, _))
+  }
+
+  def fold[A, B](t: BTree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
   }
 }
 
