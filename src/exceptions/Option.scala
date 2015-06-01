5c0135b2394af0e1c4f660591cf8b1eed7df8b29
Ashok Gelal <ashokgelal@gmail.com>
2015-05-31 23:36:04 -0600

    Add Option

---
+++ src/exceptions/Option.scala
@@ -0,0 +1,55 @@
+package exceptions
+
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = {
+    map(f).getOrElse(None)
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = {
+    this.map(Some(_)).getOrElse(ob)
+  }
+
+  def filter(f: A => Boolean): Option[A] = {
+    flatMap(a => if (f(a)) Some(a) else None)
+  }
+
+  def lift[A, B](f: A => B): Option[A] => Option[B] = {
+    _ map f
+  }
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a.flatMap(aa => b.map(bb => f(aa, bb)))
+  }
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => h.flatMap(hh => sequence(t).map(hh :: _))
+  }
+}
+
+case class Some[+A](get: A) extends Option[A]
+
+case object None extends Option[Nothing]
+
+object Option {
+
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+}
+
+object Run {
+  def main(args: Array[String]): Unit = {
+    println(s"mean(): ${Option.mean(List(1.0, 2.0, 3.0))}")
+  }
+}
